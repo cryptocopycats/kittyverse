fa2f828f7863200df5550e6dfb4c7efc42cd413d
Gerald Bauer <gerald.bauer@gmail.com>
2019-05-03 21:13:52 +0200

    Create purrstige.rb

---
+++ lib/kittyverse/purrstige.rb
@@ -0,0 +1,127 @@
+# encoding: utf-8
+
+## Purrstige Traits  / Cattributes 
+##
+
+
+=begin
+Apr 13, 2019
+Pirate Purrstiges
+
+Scratchingpost trait is discovered. Purrstige
+  time: { end: '2019-05-10' }, traits: ['WE10', 'PU26']
+Hooked trait is discovered. Purrstige
+  time: { end: '2019-05-10' }, traits: ['WE10', 'PU27']
+Landlubber trait is discovered. Purrstige
+  time: { end: '2019-05-10' }, traits: ['WE10', 'PU29']
+Wrecked trait is discovered. Purrstige
+  time: { end: '2019-05-10' }, traits: ['EN00', 'PU26']
+Purrbados trait is discovered. Purrstige
+  time: { end: '2019-05-10' }, traits: ['EN01', 'PU26']
+Timbers trait is discovered. Purrstige
+  time: { end: '2019-05-10' }, traits: ['WE10', 'EN00', 'PU26']
+Maraud trait is discovered. Purrstige
+  time: { end: '2019-05-10' }, traits: ['WE10', 'EN01', 'PU26']
+
+
+# Mar 15, 2019
+# Oohshiny trait is discovered. Purrstige
+oohshiny: { name: 'Oohshiny', limit: 2484,
+   time: { start: '2019-03-15', end: '2019-03-28'},
+   traits: ['roadtogold', 'PU27']
+
+
+Feb 21, 2019
+Alpacat purrstiges
+Dreamcloud trait is discovered.Purrstige
+  time: { start: '2019-02-22', end: '2019-03-25' }, limit: 1246,
+  traits: ['munchkin', 'WE10', 'PU24'],
+Alpunka trait is discovered.Purrstige
+  time: { start: '2019-02-22', end: '2019-03-25' }, limit: 926,
+  traits: ['munchkin', 'WE10', 'PU28'],
+Cindylou trait is discovered.Purrstige
+  time: { start: '2019-02-22', end: '2019-03-25' }, limit: 905,
+  traits: ['munchkin', 'WE10', 'PU30'],
+Alpacacino trait is discovered.Purrstige
+  time: { start: '2019-02-22', end: '2019-03-25' }, limit: 1220,
+  traits: ['munchkin', 'WE10', 'PU27'],
+Beatlesque trait is discovered.Purrstige
+  time: { start: '2019-02-22', end: '2019-03-25' }, limit: 783,
+  traits: ['munchkin', 'WE10', 'PU29'],
+Huacool trait is discovered.Purrstige
+  time: { start: '2019-02-22', end: '2019-03-25' }, limit: 837,
+  traits: ['munchkin', 'WE10', 'PU25'],
+Inaband trait is discovered.Purrstige
+  time: { start: '2019-02-22', end: '2019-03-25' }, limit: 1048,
+  traits: ['munchkin', 'WE10', 'PU26'],
+=end
+
+
+
+PURRSTIGES =
+{
+## May 1, 2019
+## 7 Robokitty Purrstiges
+bionic: { name: 'Bionic',
+          time: { start: '2019-05-01', end: '2019-08-31' },
+          traits: ['ragdoll', 'WE05', 'FIXME:Any Totesbasic', 'PU30'], ## fix: anytotesbasic!!!
+          prestige: true },
+fileshare: { name: 'Fileshare',
+             time: { start: '2019-05-01', end: '2019-08-31' },
+             traits: ['ragdoll', 'WE05', 'FIXME:Any Totesbasic', 'PU29'],
+             prestige: true },
+pawsfree:  { name: 'Pawsfree',
+             time: { start: '2019-05-01', end: '2019-08-31' },
+             traits: ['ragdoll', 'WE05', 'FIXME:Any Totesbasic', 'PU28'],
+             prestige: true },
+uplink:  { name: 'Uplink',
+           time: { start: '2019-05-01', end: '2019-08-31' },
+           traits: ['ragdoll', 'WE05', 'FIXME:Any Totesbasic', 'PU27'],
+           prestige: true },
+gaunlet: { name: 'Gauntlet',
+           time: { start: '2019-05-01', end: '2019-08-31' },
+           traits: ['ragdoll', 'WE05', 'FIXME:Any Totesbasic', 'PU26'],
+           prestige: true },
+catterypack: { name: 'Catterypack',
+               time: { start: '2019-05-01', end: '2019-08-31' },
+               traits: ['ragdoll', 'WE05', 'FIXME:Any Totesbasic', 'PU25'],
+               prestige: true },
+squelch: { name: 'Squelch',
+            time: { start: '2019-05-01', end: '2019-08-31' },
+            traits: ['ragdoll', 'WE05',  'FIXME:Any Totesbasic', 'PU24'],
+            prestige: true },
+
+  ## 2018
+  ## December
+
+ reindeer: { name: 'Reindeer', date: '2018-12-14',
+              limit: 854, time: { start: '2018-12-14', end: '2019-01-07' },
+              prestige: true,
+              traits: ['elk', ['PU24','PU25','PU26','PU27']] },
+ holidaycheer: { name: 'Holidaycheer', date: '2018-12-14',
+                limit: 759, time: { start: '2018-12-14', end: '2019-01-07' },
+                prestige: true,
+                traits: ['elk', ['PU28','PU29']] },
+  lit:      { name: 'Lit', date: '2018-12-14',
+              limit: 1006, time: { start: '2018-12-14', end: '2019-01-07' },
+              prestige: true,
+               traits: ['elk', 'PU30'] },
+               
+## October
+  
+  thatsawrap:  { name: 'Thatsawrap', date: '2018-10-21', limit:	615, time: { start: '2018-10-20', end: '2018-11-06' }, prestige: true,
+                  traits: ['bobtail','WE00','PU28']},
+  duckduckcat: { name: 'Duckduckcat', date: '2018-10-19', limit: 1249, time: { start: '2018-10-20', end: '2018-11-15' }, prestige: true,
+                   traits: ['neckbeard',['PU24','PU25','PU26']]},  ##  Purrstige: PU24 / PU25 / PU26
+
+## September
+
+  prune:   { name: 'Prune', date: '2018-09-19', 
+               limit: 921, time: { start: '2018-09-18', end: '2018-09-30' }, prestige: true,
+            traits: ['norwegianforest', 'totesbasic', 'PU25']},     ## todo/fix: check totesbasic - three genes!!! - check which one PA15?
+  furball:  { name: 'Furball', date: '2018-09-19', 
+                limit: 998, time: { start: '2018-09-19', end: '2018-09-30' }, prestige: true,
+              traits: ['norwegianforest', 'totesbasic', 'PU26']},   ## todo/fix: check totesbasic - three genes!!! - check which one PA15?
+  
+}
+
