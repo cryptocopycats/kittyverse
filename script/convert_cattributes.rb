6105573ea088e7f637944a2f586ee4cdff9081c5
Gerald Bauer <gerald.bauer@gmail.com>
2019-05-06 19:17:55 +0200

    Create convert_cattributes.rb

---
+++ script/convert_cattributes.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+###
+# see https://api.cryptokitties.co/cattributes
+
+require 'json'
+require 'pp'
+
+txt = File.read( './dl/cattributes-2019-05-06.json' )
+
+data = JSON.parse( txt )
+## pp data
+
+data = data.reverse  ## note: list most rare first / most popular last
+
+
+recs = []
+data.each do |row|
+  rec = []
+
+  rec << row['total']
+  rec << row['description']
+  rec << row['type']
+
+  recs << rec
+end
+
+pp recs
+
+
+csv = ""
+
+csv << ("%-6s, %-15s, %-14s" % ['Total', 'Description', 'Type'])
+csv << "\n"
+recs.each_with_index do |rec,i|
+  csv << ("%6s, %-15s, %-14s" % rec)
+  csv << "\n"
+  ## break if i > 5
+end
+
+
+File.open( "./datasets/cattributes.csv", "w:utf-8" ) do |f|
+  f.write csv
+end
