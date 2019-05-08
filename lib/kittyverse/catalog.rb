f2ff91967c45799b2d5232eba34d89913ae9ceb6
Gerald Bauer <gerald.bauer@gmail.com>
2019-05-08 09:42:01 +0200

    up

--- lib/kittyverse/catalog.rb
+++ lib/kittyverse/catalog.rb
@@ -3,29 +3,23 @@
 
 class Catalog    ## todo:  use a module - move to its own file -  why? why not?
 
-  @@specials   = {}  # special edition fancies
-  @@exclusives = {}  # exclusive fancies
-  @@fancies    = {}  # "normal" fancies
-
-  @@prestiges  = {}  # purrstige cattributes
+  @@special_editions   = {}  # special edition fancies
+  @@exclusives         = {}  # exclusive fancies
+  @@fancies            = {}  # "normal" fancies
 
 
   ## use special_cats, exclusive_cats, etc. - why? why not?
-  def self.specials()   @@specials; end
-  def self.exclusives() @@exclusives; end
-  def self.fancies()    @@fancies; end
-
-  ## use prestige(purrstige)_cattributes - why? why not?
-  def self.prestiges()  @@prestiges; end
+  def self.special_editions()   @@special_editions; end
+  def self.exclusives()         @@exclusives; end
+  def self.fancies()            @@fancies; end
 
 
+## auto-fill
 FANCIES.each do |key,h|
-  if h[:special]
-    specials[key] = h
+  if h[:specialedition]
+    special_editions[key] = h
   elsif h[:exclusive]
     exclusives[key] = h
-  elsif h[:prestige]
-    prestiges[key] = h
   else
     fancies[key] = h
   end
