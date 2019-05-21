0ae9bf71fca1c8df7038012f3b4c98a48b3fde95
Gerald Bauer <gerald.bauer@gmail.com>
2019-05-21 16:56:56 +0200

    Create test_genome.rb

---
+++ test/test_genome.rb
@@ -0,0 +1,62 @@
+
+pp genome[0].class
+pp genome[0].name
+pp genome[0].code
+
+pp genome[5].class
+pp genome[5].name
+pp genome[5].code
+
+pp genome[40].class
+pp genome[40].name
+pp genome[40].code
+
+
+pp genome.body.class
+pp genome.body.type.name
+pp genome.body.type.code
+
+pp genome[:body].class
+pp genome[:body].type.name
+pp genome[:body].type.code
+
+pp genome.fu.class
+pp genome.fu.type.name
+pp genome.fu.type.code
+
+pp genome[:FU].class
+pp genome[:FU].type.name
+pp genome[:FU].type.code
+
+pp genome.body.d.class
+pp genome.body.d.name
+pp genome.body.d.code
+
+pp genome[:body].d.class
+pp genome[:body].d.name
+pp genome[:body].d.code
+
+pp genome.fu.d.class
+pp genome.fu.d.name
+pp genome.fu.d.code
+
+pp genome.fu.r1.class
+pp genome.fu.r1.name
+pp genome.fu.r1.code
+
+pp genome.body[0].class
+pp genome.body[0].name
+pp genome.body[0].code
+
+pp genome.body[1].class
+pp genome.body[1].name
+pp genome.body[1].code
+
+pp genome.fu[0].class
+pp genome.fu[0].name
+pp genome.fu[0].code
+
+pp genome[:body][0].class
+pp genome[:body][0].name
+pp genome[:body][0].code
+
